c858b52f1f1636bc0e62c16f6cafda239b4b84f3
ivanrmnvch <ivansylkin@gmail.com>
2023-10-28 14:39:14 +0300

    init app

---
+++ src/main/java/com/example/hw_store_spring/HwStoreSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.hw_store_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwStoreSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwStoreSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw_store_spring/HwStoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_store_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwStoreSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
